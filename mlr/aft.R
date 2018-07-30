3915464c8110cc6b19ab5a93f51f7e9cb8afffbc
CAJan93 <jan.mensch@gmx.net>
2018-07-30 11:53:40 +0200

    save

--- mlr/aft.R
+++ mlr/aft.R
@@ -31,47 +31,132 @@ myCindex <- function(predicted, actual) {
   return(sum / comparissons)
 }
 
-# get data
-data("kidtran")
-kidtran = na.omit(kidtran) # remove rows with missing values
-n = nrow(kidtran)
-kidtran <- kidtran[sample(n),] # shuffle row-wise
-kidtran.train = kidtran[1:(n * 0.8),]
-kidtran.test = kidtran[(n * 0.8):n,]
+# telco_churn = read.csv("telco_customer_churn.csv")  # read csv file
+telco_churn = read.csv("https://community.watsonanalytics.com/wp-content/uploads/2015/03/WA_Fn-UseC_-Telco-Customer-Churn.csv")
 
-kidtransurv <- Surv(kidtran.train$time, kidtran.train$delta)
+telco_churn = na.omit(telco_churn) # remove rows with missing values
+colnames(telco_churn)[6] <- "time"
+n = nrow(telco_churn)
+telco_churn <- telco_churn[sample(n),] # shuffle row-wise
+#telco_churn.train = sample(n, size = 0.8*n) 
+#telco_churn.test = setdiff(1:n, telco_churn.train)
+telco_churn.train_seq = seq(round(n*0.8))
+telco_churn.test_seq  = seq(from = round(n*0.8) + 1, to = n)
+#telco_churn.train = telco_churn[1:round(telco_churn.train_seq),]
+#telco_churn.test = telco_churn[round(n*0.8) + 1 : n,]
+telco_churn$Churn <- as.character(telco_churn$Churn)
+telco_churn$Churn[telco_churn$Churn == "Yes"] <- T
+telco_churn$Churn[telco_churn$Churn == "No"] <- F
+telco_churn$Churn <- as.logical(telco_churn$Churn)
 
+telco_churn_cor = telco_churn
+
+# have a look at the correlations between user attributes
+#telco_churn_cor = telco_churn[which(telco_churn$Churn == T),]
+telco_churn_cor$PhoneService <- as.character(telco_churn_cor$PhoneService)
+telco_churn_cor$PhoneService[telco_churn_cor$PhoneService == "Yes"] <- 1
+telco_churn_cor$PhoneService[telco_churn_cor$PhoneService == "No"] <- 0
+telco_churn_cor$PhoneService <- as.numeric(telco_churn_cor$PhoneService)
+
+telco_churn_cor$MultipleLines <- as.character(telco_churn_cor$MultipleLines)
+telco_churn_cor$MultipleLines[telco_churn_cor$MultipleLines == "Yes"] <- 2
+telco_churn_cor$MultipleLines[telco_churn_cor$MultipleLines == "No"] <- 1
+telco_churn_cor$MultipleLines[telco_churn_cor$MultipleLines == "No phone service"] <- 0
+telco_churn_cor$MultipleLines <- as.numeric(telco_churn_cor$MultipleLines)
+
+telco_churn_cor$OnlineSecurity <- as.character(telco_churn_cor$OnlineSecurity)
+telco_churn_cor$OnlineSecurity[telco_churn_cor$OnlineSecurity == "Yes"] <- 2
+telco_churn_cor$OnlineSecurity[telco_churn_cor$OnlineSecurity == "No"] <- 1
+telco_churn_cor$OnlineSecurity[telco_churn_cor$OnlineSecurity == "No internet service"] <- 0
+telco_churn_cor$OnlineSecurity <- as.numeric(telco_churn_cor$OnlineSecurity)
+
+telco_churn_cor$OnlineBackup <- as.character(telco_churn_cor$OnlineBackup)
+telco_churn_cor$OnlineBackup[telco_churn_cor$OnlineBackup == "Yes"] <- 2
+telco_churn_cor$OnlineBackup[telco_churn_cor$OnlineBackup == "No"] <- 1
+telco_churn_cor$OnlineBackup[telco_churn_cor$OnlineBackup == "No internet service"] <- 0
+telco_churn_cor$OnlineBackup <- as.numeric(telco_churn_cor$OnlineBackup)
+
+telco_churn_cor$DeviceProtection <- as.character(telco_churn_cor$DeviceProtection)
+telco_churn_cor$DeviceProtection[telco_churn_cor$DeviceProtection == "Yes"] <- 2
+telco_churn_cor$DeviceProtection[telco_churn_cor$DeviceProtection == "No"] <- 1
+telco_churn_cor$DeviceProtection[telco_churn_cor$DeviceProtection == "No internet service"] <- 0
+telco_churn_cor$DeviceProtection <- as.numeric(telco_churn_cor$DeviceProtection)
+
+telco_churn_cor$StreamingTV <- as.character(telco_churn_cor$StreamingTV)
+telco_churn_cor$StreamingTV[telco_churn_cor$StreamingTV == "Yes"] <- 1
+telco_churn_cor$StreamingTV[telco_churn_cor$StreamingTV == "No"] <- 0
+telco_churn_cor$StreamingTV[telco_churn_cor$StreamingTV == "No internet service"] <- 0
+telco_churn_cor$StreamingTV <- as.numeric(telco_churn_cor$StreamingTV)
+
+telco_churn_cor$StreamingMovies <- as.character(telco_churn_cor$StreamingMovies)
+telco_churn_cor$StreamingMovies[telco_churn_cor$StreamingMovies == "Yes"] <- 2
+telco_churn_cor$StreamingMovies[telco_churn_cor$StreamingMovies == "No"] <- 1
+telco_churn_cor$StreamingMovies[telco_churn_cor$StreamingMovies == "No internet service"] <- 0
+telco_churn_cor$StreamingMovies <- as.numeric(telco_churn_cor$StreamingMovies)
+
+telco_churn_cor$TechSupport <- as.character(telco_churn_cor$TechSupport)
+telco_churn_cor$TechSupport[telco_churn_cor$TechSupport == "Yes"] <- 2
+telco_churn_cor$TechSupport[telco_churn_cor$TechSupport == "No"] <- 1
+telco_churn_cor$TechSupport[telco_churn_cor$TechSupport == "No internet service"] <- 0
+telco_churn_cor$TechSupport <- as.numeric(telco_churn_cor$TechSupport)
+
+telco_churn_cor$InternetService <- as.character(telco_churn_cor$InternetService)
+telco_churn_cor$InternetService[telco_churn_cor$InternetService == "Fiber optic"] <- 2
+telco_churn_cor$InternetService[telco_churn_cor$InternetService == "DSL"] <- 1
+telco_churn_cor$InternetService[telco_churn_cor$InternetService == "No"] <- 0
+telco_churn_cor$InternetService <- as.numeric(telco_churn_cor$InternetService)
+
+
+telco_churn_cor$PaperlessBilling <- as.character(telco_churn_cor$PaperlessBilling)
+telco_churn_cor$PaperlessBilling[telco_churn_cor$PaperlessBilling == "Yes"] <- 1
+telco_churn_cor$PaperlessBilling[telco_churn_cor$PaperlessBilling == "No"] <- 0
+telco_churn_cor$PaperlessBilling <- as.numeric(telco_churn_cor$PaperlessBilling)
+
+
+telco_churn_cor$Partner <- as.character(telco_churn_cor$Partner)
+telco_churn_cor$Partner[telco_churn_cor$Partner == "Yes"] <- 1
+telco_churn_cor$Partner[telco_churn_cor$Partner == "No"] <- 0
+telco_churn_cor$Partner <- as.numeric(telco_churn_cor$Partner)
+
+telco_churn_cor$Dependents <- as.character(telco_churn_cor$Dependents)
+telco_churn_cor$Dependents[telco_churn_cor$Dependents == "Yes"] <- 1
+telco_churn_cor$Dependents[telco_churn_cor$Dependents == "No"] <- 0
+telco_churn_cor$Dependents <- as.numeric(telco_churn_cor$Dependents)
+
+telco_churn_cor$Contract <- as.character(telco_churn_cor$Contract)
+telco_churn_cor$Contract[telco_churn_cor$Contract == "One year"] <- 1
+telco_churn_cor$Contract[telco_churn_cor$Contract == "Two year"] <- 2
+telco_churn_cor$Contract[telco_churn_cor$Contract == "Month-to-month"] <- 0
+telco_churn_cor$Contract <- as.numeric(telco_churn_cor$Contract)
+
+fit <- Surv(telco_churn_cor$time, telco_churn_cor$Churn)
 
 library(flexsurv)
-plot(flexsurvreg(kidtransurv~1, dist="exp")) # bad 
-plot(flexsurvreg(kidtransurv~1, dist="weibull")) # good
-plot(flexsurvreg(kidtransurv~1, dist="gamma")) # good
-plot(flexsurvreg(kidtransurv~1, dist="gengamma")) # ok
-plot(flexsurvreg(kidtransurv~1, dist="genf")) # bad
-plot(flexsurvreg(kidtransurv~1, dist="lnorm")) # ok 
-plot(flexsurvreg(kidtransurv~1, dist="gompertz")) # ok
+plot(flexsurvreg(fit~1, dist="weibull"), main = "weibull", ylab="S(t)", xlab="t")  # good
+plot(flexsurvreg(fit~1, dist="exponential"), main = "exponential", ylab="S(t)", xlab="t") # bad
+plot(flexsurvreg(fit~1, dist="lognormal"), main = "lognormal", ylab="S(t)", xlab="t") # ok
 
-# booth weibull and gamma distributions seem to fit good 
-aftmodel  <- survreg(Surv(time, delta) ~ gender + race + age, dist = "weibull", data = kidtran.train)
+plot(flexsurvreg(fit~1, dist="gengamma"), main = "gengamma", ylab="S(t)", xlab="t") # ok
+plot(flexsurvreg(fit~1, dist="genf"), main = "genf", ylab="S(t)", xlab="t") # ok
+plot(flexsurvreg(fit~1, dist="llogis"), main = "llogis", ylab="S(t)", xlab="t") # ok
+plot(flexsurvreg(fit~1, dist="gompertz"), main = "gompertz", ylab="S(t)", xlab="t") # ok
 
-# flexAFT<-flexsurvreg(kidtransurv~kidtran.train$gender+kidtran.train$race+kidtran.train$age, dist = "weibull")
-# plot(flexAFT, type="survival")
 
-# now predict values with aftmodel
-# why does this predict on the old data, not the new data? 
-predicted <- predict(aftmodel, newdata = kidtran.test)
-# remove all censored data and only use observed time
-actual <- kidtran.test[kidtran.test$delta != 0,]$time
-predicted <- predicted[kidtran.test$delta != 0]
-# remove elements, where predicted is NA
-actual <- actual[!is.na(predicted)]
-predicted <- predicted[!is.na(predicted)]
 
-print(myCindex(predicted = as.vector(predicted), actual = actual)) # 0.4927536 # 0.60 ??? 
 
-# uno c-index, does not consider the underlying distribution
-predicted <- predict(aftmodel, newdata = kidtran.test)
-kidtransurv.test <- Surv(kidtran.test$time, kidtran.test$delta)
-# survAUC::UnoC(Surv.rsp = flexAFT, Surv.rsp.new = flexAFT, predicted ) # 0.5286549
-survAUC::UnoC(Surv.rsp = kidtransurv, Surv.rsp.new = kidtransurv.test, predicted ) # 0.5286549
+telco_churn_cor$Churn <- as.logical(telco_churn_cor$Churn)
+
+# booth weibull and lognormal distributions seem to fit good 
+aftmodelWeibul  <- survreg(Surv(time, Churn) ~ gender + SeniorCitizen + Partner + Dependents + PhoneService + MultipleLines + InternetService + OnlineSecurity + OnlineBackup + DeviceProtection + TechSupport + StreamingTV + StreamingMovies + Contract + PaperlessBilling + PaymentMethod + MonthlyCharges, dist = "weibull", data = telco_churn_cor[telco_churn.train_seq,])
+predicted <- predict(aftmodelWeibul, newdata = telco_churn_cor[telco_churn.test_seq,])
+actual    <- telco_churn_cor[telco_churn.test_seq,]$time
+predicted <- as.vector(predicted)
+print(myCindex(predicted = predicted, actual = actual)) # 0.7545345 # 0.7604157 # 0.75653 # 0.7495361 # 0.7521168 # 0.7493649 # 0.7608156 # 0.7427811 # 0.7625509 # 0.7467842
+
+
+aftmodelln   <- survreg(Surv(time, Churn) ~ gender + SeniorCitizen + Partner + Dependents + PhoneService + MultipleLines + InternetService + OnlineSecurity + OnlineBackup + DeviceProtection + TechSupport + StreamingTV + StreamingMovies + Contract + PaperlessBilling + PaymentMethod + MonthlyCharges, dist = "lognormal", data = telco_churn_cor[telco_churn.train_seq,])
+predicted <- predict(aftmodelln, newdata = telco_churn_cor[telco_churn.test_seq,])
+actual    <- telco_churn_cor[telco_churn.test_seq,]$time
+predicted <- as.vector(predicted)
+print(myCindex(predicted = predicted, actual = actual)) # 0.7622482 # 0.768333 # 0.7628415 # 0.7572691 # 0.7595177 # 0.7561949 # 0.7687329 # 0.751408 # 0.7698456 # 0.7566343
 
--- mlr/coxPH.R
+++ mlr/coxPH.R
@@ -12,7 +12,7 @@
 # remove objects in session with rm(list = ls(all = TRUE))
 
 # install and load required packages, if not installed already
-list.of.packages <- c("mlr", "survival", "CoxBoost", "party", "KMsurv", "survAUC")
+list.of.packages <- c("survminer","ggplot2","mlr", "survival", "CoxBoost", "party", "KMsurv", "survAUC")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if (length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only = TRUE)
@@ -20,83 +20,196 @@ lapply(list.of.packages, require, character.only = TRUE)
 
 
 # import and process data
-data("kidtran") # get data
-kidtran = na.omit(kidtran) # remove rows with missing values
-kidtran$delta = (kidtran$delta == 1) # convert to logical
-n = nrow(kidtran)
-kidtran <- kidtran[sample(n),] # shuffle row-wise
-kidtran.train = sample(n, size = 0.8*n) 
-kidtran.test = setdiff(1:n, kidtran.train)
-# kidtran.train = kidtran[1:round(n*0.8),]
-# kidtran.test = kidtran[round(n*0.8) + 1 : n,]
+# telco_churn = read.csv("telco_customer_churn.csv")  # read csv file
+telco_churn = read.csv("https://community.watsonanalytics.com/wp-content/uploads/2015/03/WA_Fn-UseC_-Telco-Customer-Churn.csv")
+
+telco_churn = na.omit(telco_churn) # remove rows with missing values
+colnames(telco_churn)[6] <- "time"
+n = nrow(telco_churn)
+telco_churn <- telco_churn[sample(n),] # shuffle row-wise
+#telco_churn.train = sample(n, size = 0.8*n) 
+#telco_churn.test = setdiff(1:n, telco_churn.train)
+telco_churn.train_seq = seq(round(n*0.8))
+telco_churn.test_seq  = seq(from = round(n*0.8) + 1, to = n)
+#telco_churn.train = telco_churn[1:round(telco_churn.train_seq),]
+#telco_churn.test = telco_churn[round(n*0.8) + 1 : n,]
+telco_churn$Churn <- as.character(telco_churn$Churn)
+telco_churn$Churn[telco_churn$Churn == "Yes"] <- T
+telco_churn$Churn[telco_churn$Churn == "No"] <- F
+telco_churn$Churn <- as.logical(telco_churn$Churn)
+
+telco_churn_cor = telco_churn
+
+# have a look at the correlations between user attributes
+#telco_churn_cor = telco_churn[which(telco_churn$Churn == T),]
+telco_churn_cor$PhoneService <- as.character(telco_churn_cor$PhoneService)
+telco_churn_cor$PhoneService[telco_churn_cor$PhoneService == "Yes"] <- 1
+telco_churn_cor$PhoneService[telco_churn_cor$PhoneService == "No"] <- 0
+telco_churn_cor$PhoneService <- as.numeric(telco_churn_cor$PhoneService)
+
+telco_churn_cor$MultipleLines <- as.character(telco_churn_cor$MultipleLines)
+telco_churn_cor$MultipleLines[telco_churn_cor$MultipleLines == "Yes"] <- 2
+telco_churn_cor$MultipleLines[telco_churn_cor$MultipleLines == "No"] <- 1
+telco_churn_cor$MultipleLines[telco_churn_cor$MultipleLines == "No phone service"] <- 0
+telco_churn_cor$MultipleLines <- as.numeric(telco_churn_cor$MultipleLines)
+
+telco_churn_cor$OnlineSecurity <- as.character(telco_churn_cor$OnlineSecurity)
+telco_churn_cor$OnlineSecurity[telco_churn_cor$OnlineSecurity == "Yes"] <- 2
+telco_churn_cor$OnlineSecurity[telco_churn_cor$OnlineSecurity == "No"] <- 1
+telco_churn_cor$OnlineSecurity[telco_churn_cor$OnlineSecurity == "No internet service"] <- 0
+telco_churn_cor$OnlineSecurity <- as.numeric(telco_churn_cor$OnlineSecurity)
+
+telco_churn_cor$OnlineBackup <- as.character(telco_churn_cor$OnlineBackup)
+telco_churn_cor$OnlineBackup[telco_churn_cor$OnlineBackup == "Yes"] <- 2
+telco_churn_cor$OnlineBackup[telco_churn_cor$OnlineBackup == "No"] <- 1
+telco_churn_cor$OnlineBackup[telco_churn_cor$OnlineBackup == "No internet service"] <- 0
+telco_churn_cor$OnlineBackup <- as.numeric(telco_churn_cor$OnlineBackup)
+
+telco_churn_cor$DeviceProtection <- as.character(telco_churn_cor$DeviceProtection)
+telco_churn_cor$DeviceProtection[telco_churn_cor$DeviceProtection == "Yes"] <- 2
+telco_churn_cor$DeviceProtection[telco_churn_cor$DeviceProtection == "No"] <- 1
+telco_churn_cor$DeviceProtection[telco_churn_cor$DeviceProtection == "No internet service"] <- 0
+telco_churn_cor$DeviceProtection <- as.numeric(telco_churn_cor$DeviceProtection)
+
+telco_churn_cor$StreamingTV <- as.character(telco_churn_cor$StreamingTV)
+telco_churn_cor$StreamingTV[telco_churn_cor$StreamingTV == "Yes"] <- 1
+telco_churn_cor$StreamingTV[telco_churn_cor$StreamingTV == "No"] <- 0
+telco_churn_cor$StreamingTV[telco_churn_cor$StreamingTV == "No internet service"] <- 0
+telco_churn_cor$StreamingTV <- as.numeric(telco_churn_cor$StreamingTV)
+
+telco_churn_cor$StreamingMovies <- as.character(telco_churn_cor$StreamingMovies)
+telco_churn_cor$StreamingMovies[telco_churn_cor$StreamingMovies == "Yes"] <- 2
+telco_churn_cor$StreamingMovies[telco_churn_cor$StreamingMovies == "No"] <- 1
+telco_churn_cor$StreamingMovies[telco_churn_cor$StreamingMovies == "No internet service"] <- 0
+telco_churn_cor$StreamingMovies <- as.numeric(telco_churn_cor$StreamingMovies)
+
+telco_churn_cor$TechSupport <- as.character(telco_churn_cor$TechSupport)
+telco_churn_cor$TechSupport[telco_churn_cor$TechSupport == "Yes"] <- 2
+telco_churn_cor$TechSupport[telco_churn_cor$TechSupport == "No"] <- 1
+telco_churn_cor$TechSupport[telco_churn_cor$TechSupport == "No internet service"] <- 0
+telco_churn_cor$TechSupport <- as.numeric(telco_churn_cor$TechSupport)
+
+telco_churn_cor$InternetService <- as.character(telco_churn_cor$InternetService)
+telco_churn_cor$InternetService[telco_churn_cor$InternetService == "Fiber optic"] <- 2
+telco_churn_cor$InternetService[telco_churn_cor$InternetService == "DSL"] <- 1
+telco_churn_cor$InternetService[telco_churn_cor$InternetService == "No"] <- 0
+telco_churn_cor$InternetService <- as.numeric(telco_churn_cor$InternetService)
+
+
+telco_churn_cor$PaperlessBilling <- as.character(telco_churn_cor$PaperlessBilling)
+telco_churn_cor$PaperlessBilling[telco_churn_cor$PaperlessBilling == "Yes"] <- 1
+telco_churn_cor$PaperlessBilling[telco_churn_cor$PaperlessBilling == "No"] <- 0
+telco_churn_cor$PaperlessBilling <- as.numeric(telco_churn_cor$PaperlessBilling)
+
+
+telco_churn_cor$Partner <- as.character(telco_churn_cor$Partner)
+telco_churn_cor$Partner[telco_churn_cor$Partner == "Yes"] <- 1
+telco_churn_cor$Partner[telco_churn_cor$Partner == "No"] <- 0
+telco_churn_cor$Partner <- as.numeric(telco_churn_cor$Partner)
+
+telco_churn_cor$Dependents <- as.character(telco_churn_cor$Dependents)
+telco_churn_cor$Dependents[telco_churn_cor$Dependents == "Yes"] <- 1
+telco_churn_cor$Dependents[telco_churn_cor$Dependents == "No"] <- 0
+telco_churn_cor$Dependents <- as.numeric(telco_churn_cor$Dependents)
+
+telco_churn_cor$Contract <- as.character(telco_churn_cor$Contract)
+telco_churn_cor$Contract[telco_churn_cor$Contract == "One year"] <- 1
+telco_churn_cor$Contract[telco_churn_cor$Contract == "Two year"] <- 2
+telco_churn_cor$Contract[telco_churn_cor$Contract == "Month-to-month"] <- 0
+telco_churn_cor$Contract <- as.numeric(telco_churn_cor$Contract)
+
+
+if (T){
+# KM
+library(survminer)
+fit <- survfit(Surv(time, Churn) ~ 1,
+               data = telco_churn_cor)
+# Visualize with survminer
+ggsurvplot(fit, data = telco_churn_cor, risk.table = TRUE, title = "Kaplan-Meier curve", ylim = c(0.50, 1), palette="#C92A2F")
+}
 
 
 
+# works
+cor(telco_churn_cor[, -which(names(telco_churn_cor) %in% c("Churn","customerID","time",  "PaymentMethod", "gender", "Dependents"))], telco_churn_cor["time"], method = "spearman")
+
+telco_churn_cor$Churn <- as.logical(telco_churn_cor$Churn)
+if (F) {
 # cox proportional hazard
 coxph.lrn = makeLearner("surv.coxph") 
-coxph.tsk = makeSurvTask(data = kidtran, target = c("time", "delta"))
-coxph.mod = train(coxph.lrn, coxph.tsk, subset = kidtran.train) 
-coxph.tsk.pred = predict(coxph.mod, task = coxph.tsk, subset = kidtran.test)
+coxph.tsk = makeSurvTask(id = "telco_churn_cor", data = telco_churn_cor[, -which(names(telco_churn_cor) %in% c("TotalCharges", "customerID"))], target = c("time", "Churn"))
+coxph.mod = train(coxph.lrn, coxph.tsk, subset = telco_churn.train_seq) 
+coxph.tsk.pred = predict(coxph.mod, task = coxph.tsk, subset = telco_churn.test_seq)
 # Does this give me how the risk of a person 
-# is different from the avg. risk? 
-exp(coxph.tsk.pred$data$response)
 # possible performance measures
 listMeasures(coxph.tsk)
 # what does this tell me? 
-performance(coxph.tsk.pred, measures = mlr::cindex) # 0.9131075 
-performance(coxph.tsk.pred, measure = cindex.uno, model = coxph.mod, task = coxph.tsk) # 0.8663421 
-performance(coxph.tsk.pred, measures = ibrier, model = coxph.mod, task = coxph.tsk) # 0.144805 
+performance(coxph.tsk.pred, measures = mlr::cindex) # 0.8728485 # 0.8473736 # 0.8716186 # 0.8677882 # 0.8595095 # 0.8572907 # 0.8647923 # 0.8683304 # 0.869991 # 0.8578821 # avg: 0.8637424899999999
+performance(coxph.tsk.pred, measure = cindex.uno, model = coxph.mod, task = coxph.tsk) # 0.8623853 # 0.9268293 # 0.8461538 # 0.9864865 # 0.9418605 # 0.8888889 # 0.99999 # 0.6666667 # 0.7565789 # 0.9345238 # avg: 0.88103637
+performance(coxph.tsk.pred, measures = ibrier, model = coxph.mod, task = coxph.tsk) # 0.178092 # 0.1753829 # 0.1777078 # 0.1769075 # 0.1766885 # 0.1767159 # 0.1753943 # 0.1748194 # 0.1775474 # 0.1753555 # avg: 0.17646112
+}
 
 
+if (F){
 
 # cox proportional hazard with Componentwise Likelihood based Boosting
 coxboost.lrn = makeLearner("surv.CoxBoost") 
-coxboost.tsk = makeSurvTask(data = kidtran, target = c("time", "delta"))
-coxboost.mod = train(coxboost.lrn, coxboost.tsk, subset = kidtran.test) 
-coxboost.tsk.pred = predict(coxboost.mod, task = coxboost.tsk, subset = kidtran.test)
+coxboost.tsk = makeSurvTask(id = "telco_churn_cor" ,data = telco_churn_cor[, -which(names(telco_churn_cor) %in% c("TotalCharges", "customerID"))], target = c("time", "Churn"))
+coxboost.mod = train(coxboost.lrn, coxboost.tsk, subset = telco_churn.train_seq) 
+coxboost.tsk.pred = predict(coxboost.mod, task = coxboost.tsk, subset = telco_churn.test_seq)
 # Does this give me how the risk of a person 
-# is different from the avg. risk? 
-exp(coxboost.tsk.pred$data$response)
 # possible performance measures
 listMeasures(coxboost.tsk)
-performance(coxboost.tsk.pred, measures = mlr::cindex) # 0.81958
-performance(coxboost.tsk.pred, measures = cindex.uno, model = coxboost.mod, task = coxboost.tsk) # 0.8456482
-performance(coxboost.tsk.pred, measures = mlr::ibrier, model = coxboost.mod, task = coxboost.tsk)
+performance(coxboost.tsk.pred, measures = mlr::cindex) # 0.8299091 # 0.8282858 # 0.8413414 # 0.8422842 # 0.8534125 # 0.8375552 # 0.8377701 # 0.8264238 # 0.8293538 # 0.8496555
+performance(coxboost.tsk.pred, measures = cindex.uno, model = coxboost.mod, task = coxboost.tsk) # 0.7511111  # 0.79375 # 0.9047619 # 0.969697 # 0.7853982 # 0.76 # 0.7969697 # 0.875 # 0.2038835 # 0.908046
+#performance(coxboost.tsk.pred, measures = ibrier, model = coxboost.mod, task = coxboost.tsk)
+# performance(coxboost.tsk.pred, measures = timepredict, model = coxboost.mod, task = coxboost.tsk)
+#performance(coxph.tsk.pred,    measures = ibrier,      model = coxph.mod,    task = coxph.tsk) 
+}
 
 
+
+
+
+
+
+
+if(F){
 
 # Cox Proportional Hazards Model with Componentwise Likelihood based Boosting,
 # tuned for the optimal number of boosting steps
 cvcoxboost.lrn = makeLearner("surv.cv.CoxBoost") 
-cvcoxboost.tsk = makeSurvTask(data = kidtran, target = c("time", "delta"))
-cvcoxboost.mod = train(cvcoxboost.lrn, cvcoxboost.tsk, subset = kidtran.train) 
-cvcoxboost.tsk.pred = predict(cvcoxboost.mod, task = cvcoxboost.tsk, subset = kidtran.test)
+cvcoxboost.tsk = makeSurvTask(id = "telco_churn_cor", data = telco_churn_cor[, -which(names(telco_churn_cor) %in% c("TotalCharges", "customerID"))], target = c("time", "Churn"))
+cvcoxboost.mod = train(cvcoxboost.lrn, cvcoxboost.tsk, subset = telco_churn.train_seq) 
+cvcoxboost.tsk.pred = predict(cvcoxboost.mod, task = cvcoxboost.tsk, subset = telco_churn.test_seq)#telco_churn.test_seq)
 
-exp(cvcoxboost.tsk.pred$data$response)
 # possible performance measures
 listMeasures(cvcoxboost.tsk)
 
-performance(cvcoxboost.tsk.pred, measures = mlr::cindex) # 0.8379178 
-performance(cvcoxboost.tsk.pred, measures = cindex.uno, model = cvcoxboost.mod, task = cvcoxboost.tsk) # 0.8461206 
-performance(cvcoxboost.tsk.pred, measures = mlr::ibrier, model = cvcoxboost.mod, task = cvcoxboost.tsk)
+performance(cvcoxboost.tsk.pred, measures = mlr::cindex) # 0.8476182
+performance(cvcoxboost.tsk.pred, measures = cindex.uno, model = cvcoxboost.mod, task = cvcoxboost.tsk) # 0.8255814
+}
 
 
+
+
+if (F){
 
 # Random Forest based on Conditional Inference Trees (with bagging)
 cforest.lrn = makeLearner("surv.cforest") 
-cforest.tsk = makeSurvTask(data = kidtran, target = c("time", "delta"))
-cforest.mod = train(cforest.lrn, cforest.tsk, subset = kidtran.train) 
+cforest.tsk = makeSurvTask(id = "telco_churn_cor", data = telco_churn_cor[, -which(names(telco_churn_cor) %in% c("TotalCharges", "customerID"))], target = c("time", "Churn"))
+cforest.mod = train(cforest.lrn, cforest.tsk, subset = telco_churn.train_seq) 
 # will give me -Inf as some results
-cforest.tsk.pred = predict(cforest.mod, task = cforest.tsk, subset = kidtran.test)
+cforest.tsk.pred = predict(cforest.mod, task = cforest.tsk, subset = telco_churn.test_seq)
 
-exp(cforest.tsk.pred$data$response)
 # possible performance measures
 listMeasures(cforest.tsk)
 
 cforest.tsk.pred$data = cforest.tsk.pred$data[cforest.tsk.pred$data$response != -Inf,]
 
-# performance
-performance(cforest.tsk.pred, measures = mlr::cindex)
-mlr::performance(cforest.tsk.pred, measures = mlr::cindex, model = cforest.mod, task = cforest.tsk) # 0.8657095
-mlr::performance(cforest.tsk.pred, measures = cindex.uno, model = cforest.mod, task = cforest.tsk) # 0.7590004 
-mlr::performance(cforest.tsk.pred, measures = ibrier, model = cforest.mod, task = cforest.tsk)
+# performance, excluding -inf
+performance(cforest.tsk.pred, measures = mlr::cindex) # 0.7069276 # 0.725202 # 0.7122673 # 0.7148753 # 0.7055392 # 0.7202865 # 0.7390247 # 0.7069126 # 0.7077237 # 0.6993006 
+mlr::performance(cforest.tsk.pred, measures = cindex.uno, model = cforest.mod, task = cforest.tsk) # 5.832794e-13 # 0.725202 # 8.737003e-15 # 6.371290e-14 # 2.387208e-16 # 1.122545e-13 # 0.8 # 0.75 # 1.535217e-16 # 0.6666667 
+
+mlr::performance(cforest.tsk.pred, measures = ibrier, model = cforest.mod, task = cforest.tsk) 
+}
